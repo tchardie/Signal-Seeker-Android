c9645012868b062084cddb7a63f9040c1a046d66
Tucker Hardie <tucker.hardie@gmail.com>
2012-04-09 18:58:53 -0400

    initial commit

---
+++ src/com/cs4900/signalseeker/SignalSeeker1Activity.java
@@ -0,0 +1,13 @@
+package com.cs4900.signalseeker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignalSeeker1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

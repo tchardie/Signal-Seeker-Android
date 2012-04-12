189b1f94445db17fe5aea5737ca513e6fa609fee
Tucker Hardie <tucker.hardie@gmail.com>
2012-04-12 10:54:25 -0400

    added overlay points to the map.not displaying respective color for strength and the info window needs to be customized more.Still no add, update, and delete.

--- src/com/cs4900/signalseeker/EntryPage.java
+++ src/com/cs4900/signalseeker/EntryPage.java
@@ -1,11 +1,19 @@
 package com.cs4900.signalseeker;
 
+import java.util.List;
+
+import com.cs4900.signalseeker.data.CustomizedOverlay;
+import com.cs4900.signalseeker.data.DataEntry;
+import com.cs4900.signalseeker.data.DataList;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -26,6 +34,8 @@ public class EntryPage extends MapActivity {
 	private MapController mapController;
 	private EditText locationText;
 	private Button submitSignalButton;
+	private List<DataEntry> list;
+	private GeoPoint point;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -34,7 +44,7 @@ public class EntryPage extends MapActivity {
 		setContentView(R.layout.main);
 
 		// make starting point
-		GeoPoint point = new GeoPoint((int) (lat * 1e6), (int) (lon * 1e6));
+		point = new GeoPoint((int) (lat * 1e6), (int) (lon * 1e6));
 
 		mapView = (MapView) findViewById(R.id.mapview);
 		mapView.setBuiltInZoomControls(true);
@@ -49,17 +59,43 @@ public class EntryPage extends MapActivity {
 		submitSignalButton.setOnClickListener(new Button.OnClickListener() {
 
 			public void onClick(View v) {
-				try {					
+				try {
 					Intent intent = new Intent(
 							Constants.INTENT_ACTION_NEW_DATA_POINT);
 					startActivity(intent);
 
 				} catch (Exception e) {
-					Log.i(Constants.LOGTAG + ": " + EntryPage.CLASSTAG, "Failed to load New Data Point page"
-							+ e.getMessage() + "]");
+					Log.i(Constants.LOGTAG + ": " + EntryPage.CLASSTAG,
+							"Failed to load New Data Point page"
+									+ e.getMessage() + "]");
 				}
 			}
 		});
+
+	}
+
+	public void onResume() {
+		super.onResume();
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		Drawable drawable = this.getResources().getDrawable(
+				R.drawable.point);
+		CustomizedOverlay itemizedOverlay = new CustomizedOverlay(drawable,
+				this);
+		list = DataList.parse(this).getAllUserEntries();
+		OverlayItem overlayItem = new OverlayItem(point, "1", "2");
+		itemizedOverlay.addOverlay(overlayItem);
+
+		//mapOverlays.add(itemizedOverlay);
+
+		for (int i = 0; i < list.size(); i++) {
+			DataEntry d = list.get(i);
+			GeoPoint p = new GeoPoint((int) (d.getLatitude() * 1e6),
+					(int) (d.getLongitude() * 1e6));
+			OverlayItem o = new OverlayItem(p, d.getLocation(), String.valueOf(d.getWifi()));
+			itemizedOverlay.addOverlay(o);
+		}
+		
+		mapOverlays.add(itemizedOverlay);
 
 	}
 
